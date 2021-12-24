f3cdab5aa2433d7d4de187f5b345069425535a61
ItsDubC <itsdubc@gmail.com>
2021-12-24 15:45:32 -0600

    Added CORS policy.  Verifying fronten pulls data successfully from backend.

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -37,6 +37,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,12 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => { x
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:4200");
+            });
 
             app.UseAuthorization();
 
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Find Bae';
+  users: any;
+
+  constructor(private http: HttpClient) 
+  {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("https://localhost:5001/api/users").subscribe(result => {
+      this.users = result;
+    })
+  }
 }
