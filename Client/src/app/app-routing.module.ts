c9ff4e20b9c7845373c683ab7324375386c2aab3
ItsDubC <itsdubc@gmail.com>
2022-01-02 07:15:29 -0600

    Added AuthGuard & protected MembersComponent

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ListsComponent } from './_components/lists/lists.component';
 import { MemberDetailComponent } from './_components/members/member-detail/member-detail.component';
 import { MemberListComponent } from './_components/members/member-list/member-list.component';
 import { MessagesComponent } from './_components/messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
+  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
   { path: 'member/:id', component: MemberDetailComponent },
   { path: 'lists', component: ListsComponent },
   { path: 'messages', component: MessagesComponent },
