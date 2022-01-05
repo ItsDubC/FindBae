16ee40463886869590d129aee613fae3cce85db7
ItsDubC <itsdubc@gmail.com>
2022-01-05 05:05:53 -0600

    Implemented age calculation extension method for DateTime.

--- Api/Entities/AppUser.cs
+++ Api/Entities/AppUser.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Extensions;
 
 namespace Api.Entities
 {
@@ -17,11 +18,16 @@ namespace Api.Entities
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
-        public string Intro { get; set; }
+        public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
---
+++ Api/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+namespace Api.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth.Date > today.AddYears(-age))
+                age--;
+
+            return age;
+        }
+    }
+}
