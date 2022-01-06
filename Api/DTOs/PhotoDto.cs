1ccbaed7cc696bbf58e60b4415bd53ce377992b5
ItsDubC <itsdubc@gmail.com>
2022-01-06 08:16:34 -0600

    Fixed PhotoDto IsPrimary.  Created client-side member and photo model interfaces.

--- Api/DTOs/PhotoDto.cs
+++ Api/DTOs/PhotoDto.cs
@@ -4,6 +4,6 @@ namespace Api.DTOs
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public bool IsMain { get; set; }
+        public bool IsPrimary { get; set; }
     }
 }
