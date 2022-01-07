eb34b94cd61839e85d4ab4a6d849caa3a7ebb5a9
ItsDubC <itsdubc@gmail.com>
2022-01-07 06:09:14 -0600

    Created Member service & wired up to MemberList component

--- Client/src/app/_components/members/member-list/member-list.component.ts
+++ Client/src/app/_components/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  members: Member[];
+  
+  constructor(private memberService: MemberService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.memberService.getMembers().subscribe(result => {
+      this.members = result;
+    })
+  }
 }
