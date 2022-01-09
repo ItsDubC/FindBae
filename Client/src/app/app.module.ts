646ea9b555172d9a1d67d4478f4d1954e58b270d
ItsDubC <itsdubc@gmail.com>
2022-01-08 20:32:59 -0600

    Implemented JWT interceptor tyo automatically handle adding bearer token to every request.  Removed manual token handling in Member service.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './_components/errors/not-found/not-found.component';
 import { ServerErrorComponent } from './_components/errors/server-error/server-error.component';
 import { MemberCardComponent } from './_components/members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { MemberCardComponent } from './_components/members/member-card/member-ca
   ],
   providers: [
     TitleCasePipe,
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
