f948fb1d96989101ab71d1dba20bafb8fd4df515
ItsDubC <itsdubc@gmail.com>
2022-01-09 19:32:45 -0600

    Wired up member update API endpoint to MemberEdit component.

--- Client/src/app/_components/members/member-edit/member-edit.component.ts
+++ Client/src/app/_components/members/member-edit/member-edit.component.ts
@@ -13,7 +13,7 @@ import { MemberService } from 'src/app/_services/member.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild("editForm") editForm : NgForm;
+  @ViewChild("editForm") editForm: NgForm;
   member: Member;
   user: User;
   @HostListener("window:beforeunload", ["$event"]) unloadNotification($event: any) {
@@ -25,11 +25,11 @@ export class MemberEditComponent implements OnInit {
   constructor(
     private accountService: AccountService,
     private memberService: MemberService,
-    private toastr: ToastrService) { 
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-        this.user = user;
-      })
-    }
+    private toastr: ToastrService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+    })
+  }
 
   ngOnInit(): void {
     this.loadMember();
@@ -42,7 +42,9 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    this.toastr.success("Profile updated", "Nice!");
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(result => {
+      this.toastr.success("Profile updated", "Nice!");
+      this.editForm.reset(this.member);
+    })
   }
 }
