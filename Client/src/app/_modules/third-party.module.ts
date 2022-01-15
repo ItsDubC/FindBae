f60a4ce6148869ba5462ccb8a95e46b004d57b9d
ItsDubC <itsdubc@gmail.com>
2022-01-15 07:42:59 -0600

    Added ng2-file uploader.  Integrated w/ member photo editor to enable photo upload functionality.

--- Client/src/app/_modules/third-party.module.ts
+++ Client/src/app/_modules/third-party.module.ts
@@ -5,6 +5,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -16,14 +17,16 @@ import { NgxSpinnerModule } from 'ngx-spinner';
       positionClass: 'toast-top-right'
     }),
     NgxGalleryModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploadModule
   ],
   exports: [
     BsDropdownModule,
     TabsModule,
     ToastrModule,
     NgxGalleryModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FileUploadModule
   ]
 })
 export class ThirdPartyModule { }
