501d13c3cb0c01d585ed5156c5d8b192908ac20e
ItsDubC <itsdubc@gmail.com>
2022-01-22 16:16:46 -0600

    Added helper and extension classes for pagination.  Added User params helper class.

---
+++ Api/Helpers/UserParams.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; }
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
