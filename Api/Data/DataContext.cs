51b7352cc6b4a7b95f9bbc7ea6a3c4d2801579b0
ItsDubC <itsdubc@gmail.com>
2022-01-26 07:12:45 -0600

    Created UserLikes entity w/ nav properties & EF migration

--- Api/Data/DataContext.cs
+++ Api/Data/DataContext.cs
@@ -16,5 +16,26 @@ namespace Api.Data
         { }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes {get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(x => new { x.SourceUserId, x.LikedUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);  // set this to DeleteBehavior.NoAction for SQL Server
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- Api/Entities/AppUser.cs
+++ Api/Entities/AppUser.cs
@@ -25,9 +25,7 @@ namespace Api.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
