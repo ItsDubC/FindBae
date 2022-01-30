5a179dc6caaf96e3b4d6078885473a1401c3b787
ItsDubC <itsdubc@gmail.com>
2022-01-30 17:44:56 -0600

    Added .NET identity configuration

--- Api/Data/DataContext.cs
+++ Api/Data/DataContext.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data
 {
-    public class DataContext : IdentityDbContext
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int, 
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, 
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         { }
@@ -15,13 +19,24 @@ namespace Api.Data
         public DataContext() : base()
         { }
 
-        public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes {get; set; }
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(x => x.UserRoles)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(x => x.UserRoles)
+                .WithOne(x => x.Role)
+                .HasForeignKey(x => x.RoleId)
+                .IsRequired();
 
             builder.Entity<UserLike>()
                 .HasKey(x => new { x.SourceUserId, x.LikedUserId });
