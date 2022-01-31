470a3138632c691b3928c79ca2210bd52a73bd47
ItsDubC <itsdubc@gmail.com>
2022-01-31 12:52:08 -0600

    Added migration for new Identity implementation

--- Api/Program.cs
+++ Api/Program.cs
@@ -25,7 +25,7 @@ namespace API
             {
                 var context = services.GetRequiredService<DataContext>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(context);
+                //await Seed.SeedUsers(context);
             }
             catch (Exception ex)
             {
