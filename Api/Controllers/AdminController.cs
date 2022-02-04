8ee3e8f09c559c856c29c734593b8822f382ae72
ItsDubC <itsdubc@gmail.com>
2022-02-03 19:26:14 -0600

    Implemented endpoint for retrieving roles

--- Api/Controllers/AdminController.cs
+++ Api/Controllers/AdminController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Api.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await _userManager.Users
+                .Include(x => x.UserRoles)
+                .ThenInclude(x => x.Role)
+                .OrderBy(x => x.UserName)
+                .Select(x => new {
+                    x.Id,
+                    Username = x.UserName,
+                    Roles = x.UserRoles.Select(x => x.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
